cb59d3f16e39ee5005cad11ab55ce8f21271dcce
tashmatov11 <tashmatovv11@gmail.com>
2022-02-10 19:51:57 +0600

    UPDATE

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -9,6 +9,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.print(bean.getMessage() + " - ");
+        System.out.println(bean == bean1);
+        System.out.print(bean2.getName() + "\t \t \t - ");
+        System.out.println(bean2 == bean3);
     }
 }
